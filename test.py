d132d89f3f9b74b820e4e6c1130b2eaa8f46c4bd
Chia-Hong Hsu <chsu35@nodec.cs.ubc.ca>
2025-09-26 14:48:48 -0700

    testing code done, generalizes

---
+++ test.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+"""
+Testing script for RecurrentEncoder with saved checkpoints.
+Loads model + optimizer (optional) and evaluates on given dataset.
+"""
+
+import argparse
+import os
+import json
+import torch
+import torch.nn as nn
+import torch.distributed as dist
+from torch.utils.data import DataLoader
+
+# Add 'src' to Python path
+import sys
+src_path = os.path.join(os.path.dirname(__file__), "src")
+if src_path not in sys.path:
+    sys.path.insert(0, src_path)
+
+from models.encoders import RecurrentEncoder
+from seq_dataset import InductionHeadDataset, SelectiveCopyDataset, infinite_dataloader
+from vae.vae import ConvVAE
+import dist_utils
+from train import setup_distributed, cleanup_distributed, evaluate, load_vae_checkpoint
+
+
+def main(args):
+    rank, world_size = setup_distributed()
+    device = torch.device(f"cuda:{rank}")
+
+    # --------------------------
+    # Load model & DDP
+    # --------------------------
+    latent_dim = 4*7*7 if args.dataset_name=="mnist" else 4*8*8
+    synth_task_rollout_len = 10 if args.synth_task=="sel_copy" else 1
+    model = RecurrentEncoder(
+        output_dim=latent_dim,
+        num_layers=args.num_layers,
+        num_heads=args.num_heads,
+        rnn_type=args.rnn_type,
+        is_video_synth_task=True,
+        video_synth_task_out_dim=10,
+        synth_task_rollout_len=synth_task_rollout_len
+    ).to(device)
+    ddp = torch.nn.parallel.DistributedDataParallel(model, device_ids=[rank])
+
+    dist_utils.print0(f"Loading checkpoint: {args.rnn_ckpt_path}")
+    checkpoint = torch.load(args.rnn_ckpt_path, map_location=device)
+    ddp.module.load_state_dict(checkpoint)
+    dist_utils.print0("Model checkpoint loaded successfully.")
+    dist_utils.print0(f"Model was trained on sequence length: {args.train_seq_len}.")
+
+    # --------------------------
+    # Setup VAE
+    # --------------------------
+    if args.dataset_name=="mnist":
+        in_ch = out_ch = 1
+    elif args.dataset_name=="cifar10":
+        in_ch = out_ch = 3
+    vae = ConvVAE(in_ch=in_ch, out_ch=out_ch, latent_ch=4, base_ch=32).to(device)
+    load_vae_checkpoint(vae, args.vae_path, device)
+    vae.eval()
+    dist_utils.print0("VAE loaded successfully.")
+
+    # --------------------------
+    # Setup Dataset + Dataloader
+    # --------------------------
+    for i in range(10,13): # [2^3, 2^9] [2^10, 2^12]
+        seq_len = 2**i
+        dist_utils.print0(f"====== ======")
+        dist_utils.print0(f"Setting up dataset + dataloader of sequence length: {2**i}.")
+        if args.synth_task == 'ind_head':
+            dataset = InductionHeadDataset(rank=rank, dataset_name=args.dataset_name, seq_len=seq_len, 
+                                        seed=args.seed+rank, root=args.dataset_root)
+        elif args.synth_task == 'sel_copy':
+            dataset = SelectiveCopyDataset(rank=rank, dataset_name=args.dataset_name, seq_len=seq_len,
+                                        seed=args.seed+rank, root=args.dataset_root, use_latent=True, vae=vae)
+        else:
+            raise ValueError(f"Unknown synthetic task {args.synth_task}")
+        inf_dataloader = infinite_dataloader(
+            DataLoader(dataset, batch_size=args.batch_size//world_size, num_workers=0, pin_memory=True)
+        )
+
+        # --------------------------
+        # Loss & Evaluation
+        # --------------------------
+        criterion = nn.CrossEntropyLoss()
+        dist_utils.print0(f"Running evaluation ...")
+        eval_loss, eval_acc, eval_batch_acc = evaluate(args, ddp, inf_dataloader, criterion, vae, world_size, device)
+        dist_utils.print0(f"[RESULT] Eval Loss: {eval_loss:.4f}, Eval Acc: {eval_acc:.4f}, Eval Batch Acc: {eval_batch_acc:.4f}")
+
+    cleanup_distributed()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluation script for trained model.")
+
+    # Dataset
+    parser.add_argument("--dataset_root", type=str, default="/ubc/cs/research/plai-scratch/chsu35/datasets")
+    parser.add_argument("--dataset_name", type=str, default="cifar10", choices=["mnist", "cifar10"])
+    parser.add_argument("--synth_task", type=str, default="ind_head", choices=["ind_head", "sel_copy"])
+    parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--seed", type=int, default=42)
+
+    # VAE
+    parser.add_argument("--vae_path", type=str, required=True, help="Path to VAE checkpoint.")
+
+    # Model checkpoint and config
+    parser.add_argument("--train_seq_len", type=int, required=True, help="Sequence length the RNN is trained on.")
+    parser.add_argument("--rnn_ckpt_path", type=str, required=True, help="Path to model checkpoint (.pt).")
+    parser.add_argument("--num_layers", type=int, default=4)
+    parser.add_argument("--num_heads", type=int, default=4)
+    parser.add_argument("--rnn_type", type=str, default="mingru")
+
+    parser.add_argument("--eval_samples", type=int, default=40)
+
+    args = parser.parse_args()
+    main(args)
+
---
+++ test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --job-name=train_rnn-test
+#SBATCH --nodes=1
+#SBATCH --ntasks=4          
+#SBATCH --gres=gpu:4       
+#SBATCH --cpus-per-task=4
+#SBATCH --time=24:00:00
+#SBATCH --partition=plai
+#SBATCH --output=logs/%x_%j.out    
+#SBATCH --error=logs/%x_%j.err   
+
+# --------- Multi gpu testing ---------
+PORT=$((10000 + RANDOM % 50000))
+
+# --------- Image ---------
+SIF_PATH="/ubc/cs/research/plai-scratch/chsu35/singularity_setup/video_synth_tasks/ubc_env.sif"
+
+PROJECT="/ubc/cs/research/fwood/chsu35/video_synthetic_tasks"
+SCRATCH="/ubc/cs/research/plai-scratch/chsu35"
+DATASET_ROOT="${SCRATCH}/datasets"
+VAE_CKPT_DIR="${SCRATCH}/vae-runs"
+RNN_CKPT_DIR="${SCRATCH}/rnn-runs" 
+
+# --------- Issue selective_scan_interface.py line#20 ---------
+HOST_PATH="${PROJECT}/src/selective_scan_interface.py"
+CONTAINER_PATH="/usr/local/lib/python3.10/dist-packages/mamba_ssm/ops/selective_scan_interface.py"
+
+DATASET="mnist" # "mnist"
+VAE_PATH="${VAE_CKPT_DIR}/vae-mnist-lr0.001-b128-kld0.0001/checkpoints/vae_epoch_300.pt"
+TRAIN_SEQ_LEN=32 # 32 / 64
+RNN_PATH="${RNN_CKPT_DIR}/seq${TRAIN_SEQ_LEN}-ind_head-mnist-mingru-ly4-b64-lr0.0001-1000k/ckpt_500000.pt"
+RNN_TYPE="mingru"
+NUM_LAYERS=4 # 2 / 4
+NUM_HEADS=4
+SYNTH_TASK="ind_head" # "ind_head" / "sel_copy"
+BATCH_SIZE=64 # 128 / 8 
+
+nvidia-smi
+
+# --------- Run Testing ----------
+echo "[INFO] Starting testing job $SLURM_JOB_ID ..."
+singularity exec --nv \
+    --bind ${HOST_PATH}:${CONTAINER_PATH} --bind ${PROJECT} --bind ${DATASET_ROOT} --bind ${VAE_CKPT_DIR} --bind ${RNN_CKPT_DIR} \
+    ${SIF_PATH} torchrun --nproc_per_node=2 test.py \
+    --train_seq_len $TRAIN_SEQ_LEN \
+    --rnn_ckpt_path $RNN_PATH \
+    --rnn_type $RNN_TYPE \
+    --num_layers $NUM_LAYERS \
+    --num_heads $NUM_HEADS \
+    --vae_path $VAE_PATH \
+    --dataset_name $DATASET \
+    --synth_task $SYNTH_TASK \
+    --batch_size $BATCH_SIZE \
+    --eval_samples $((10 * BATCH_SIZE))
